4f369d3c66d21d5aebfb3251677957bba0d1db82
Swizec Teller <swizec@swizec.com>
2023-04-01 16:39:31 -0700

    Hook up for experimentation

---
+++ src/app/TheButton.tsx
@@ -0,0 +1,16 @@
+"use client";
+import styles from "./page.module.css";
+
+export const TheButton = () => {
+    async function doTheThing() {
+        const res = await fetch("/api/the_thing").then((res) => res.text());
+
+        console.log(res);
+    }
+
+    return (
+        <button className={styles.thirteen} onClick={doTheThing}>
+            Do the thing
+        </button>
+    );
+};
